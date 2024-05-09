819e905a2e821d1365b21a3919e38f1d92caf498
bhavya <bsg@somaiya.edu>
2024-05-08 20:29:27 -0700

    add firebase hosting config and update some env variables

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,7 +1,7 @@
 import admin from "firebase-admin";
 import { getApps, initializeApp, type App } from "firebase-admin/app";
 import { getAuth, type Auth } from "firebase-admin/auth";
-import { FIREBASE_ADMIN_PRIVATE_KEY, FIREBASE_ADMIN_CLIENT_EMAIL } from "$env/static/private";
+import { FB_ADMIN_PRIVATE_KEY, FB_ADMIN_CLIENT_EMAIL } from "$env/static/private";
 import { PUBLIC_FIREBASE_PROJECT_ID } from "$env/static/public";
 
 // initialize firebase admin app
@@ -13,8 +13,8 @@ const makeApp = (): App => {
 
   return initializeApp({
     credential: admin.credential.cert({
-      privateKey: FIREBASE_ADMIN_PRIVATE_KEY,
-      clientEmail: FIREBASE_ADMIN_CLIENT_EMAIL,
+      privateKey: FB_ADMIN_PRIVATE_KEY,
+      clientEmail: FB_ADMIN_CLIENT_EMAIL,
       projectId: PUBLIC_FIREBASE_PROJECT_ID
     })
   });
